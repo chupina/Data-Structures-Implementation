beb574327d31e7e4784780789f13697097b856f8
AlenaChupina <chupina.elen@gmail.com>
2020-12-28 19:54:37 -0600

    feat: implement pop method

--- SLL.js
+++ SLL.js
@@ -1,32 +1,61 @@
 // Implementation of Singly Linked List data structure
 //defining a single node class
-class Node{
-    constructor(value){
-        this.value = value,
-        this.next = null;
-    }
+class Node {
+  constructor(value) {
+    (this.value = value), (this.next = null);
+  }
 }
 
 // defining SLL class
 
-class SinglyLinkedList{
-    constructor(){
-        this.head=null;
-        this.tail = null;
-        this.length = 0;
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+// adding new nodes to the end of a list
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
+}
 
-    // adding new nodes to the end of a list
 
-    push(val){
-        let newNode = new Node(val);
-        if (!this.head){
-            this.head = newNode;
-            this.tail = this.head;
-        }else{
-         this.tail.next = newNode;
-         this.tail = newNode;   
-        }
+    
+ 
 
+
+// removing from the end of a list
+Pop(){
+    if(!tail) return undefined;  
+    let current = this.head;
+    let newTail = current;
+    while(current.next){
+        newTail = current;
+        current = current.next;
     }
+    
+    
+   
+ 
+ 
+ this.tail = newTail;
+ this.tail.next = null;
+ this.length--;
+ if (this.length ===0){
+     this.head = null;
+     this.tail = null;
+ }
+ return current; 
 }
